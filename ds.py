16a4f79a59b24507ec20623c43072a581ecd54ea
Lorna Maria <lorna@Lornas-MacBook-Pro.local>
2022-09-29 15:05:38 -0400

    adds title to scatter plot

--- ds.py
+++ ds.py
@@ -6,4 +6,5 @@ data = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Da
 minidata = data[["Country Name", "Mortality rate, infant (per 1,000 live births)","GDP per capita (constant 2010 US$)"]]
 
 plt.scatter(minidata["Mortality rate, infant (per 1,000 live births)"],minidata["GDP per capita (constant 2010 US$)"])
+plt.title("Scatter on Mortality rate, infant (per 1,000 live births) and GDP per capita (constant 2010 US$)")
 plt.show()
