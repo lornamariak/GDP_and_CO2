23458fcaa85a6b602da7049e84c6ce68fe8271f1
Lorna Maria <lorna@Lornas-MacBook-Pro.local>
2022-09-29 14:58:18 -0400

    adds data and plots scatter

---
+++ ds.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+data = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
+minidata = data[["Country Name", "Mortality rate, infant (per 1,000 live births)","GDP per capita (constant 2010 US$)"]]
+
+plt.scatter(minidata["Mortality rate, infant (per 1,000 live births)"],minidata["GDP per capita (constant 2010 US$)"])
+plt.show()
